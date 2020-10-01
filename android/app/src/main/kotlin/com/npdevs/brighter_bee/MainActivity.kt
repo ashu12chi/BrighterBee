902bfe192128b371439a7397abe731bff694c7fb
ashu12chi <ashutosh7234@gmail.com>
2020-10-01 15:47:29 +0530

    Project Created

---
+++ android/app/src/main/kotlin/com/npdevs/brighter_bee/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.npdevs.brighter_bee
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp (
+      debugShowCheckedModeBanner: false,
+      home: Container(),
+    );
+  }
+}
