f2df664b81b653bfe54440c67364b8045fbbc5da
ashu12chi <ashutosh7234@gmail.com>
2020-10-04 18:32:18 +0530

    Create Community UI

---
+++ lib/app_screens/create_community.dart
@@ -0,0 +1,330 @@
+import 'package:flutter/material.dart';
+
+class CreateGroup extends StatefulWidget {
+  @override
+  _CreateGroupState createState() => _CreateGroupState();
+}
+
+class _CreateGroupState extends State<CreateGroup> {
+
+  int _radioValue1 = 1;
+  int _radioValue2 = 2;
+
+  void _handleRadioValueChange1(int value) {
+    setState(() {
+      _radioValue1 = value;
+
+    });
+  }
+
+  void _handleRadioValueChange2(int value) {
+    setState(() {
+      _radioValue2 = value;
+
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text(
+            'Create Community',style: TextStyle(color: Colors.black,fontSize: 20,fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+      body: ListView(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              'Name',style: TextStyle(color: Colors.black,fontSize: 20,fontWeight: FontWeight.bold),
+            ),
+          ),
+          TextField(
+            decoration: InputDecoration(
+              focusColor: Colors.blue,
+              hintText: 'Name your community',
+              hintStyle: TextStyle(color: Colors.grey,fontSize: 18),
+              border: new OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8),
+                borderSide: BorderSide(color: Colors.grey,width: 5)
+              )
+            ),
+          ),
+          Padding(
+            padding:EdgeInsets.all(8.0),
+            child:Container(
+              height:1.0,
+              width:double.infinity,
+              color:Colors.black12,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              'Cover Photo',style: TextStyle(color: Colors.black,fontSize: 20,fontWeight: FontWeight.bold),
+            ),
+          ),
+          FlatButton (
+            child: Row(
+              children: <Widget>[
+                Icon(Icons.add_box,size: 30,),
+                Padding(
+                  padding: const EdgeInsets.only(left:8.0),
+                  child: Text('Add Cover Photo',style: TextStyle(color: Colors.black,fontSize: 18),),
+                )
+              ],
+            ),
+            onPressed: (){
+
+            },
+          ),
+          Padding(
+            padding:EdgeInsets.all(8.0),
+            child:Container(
+              height:1.0,
+              width:double.infinity,
+              color:Colors.black12,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              'Privacy',style: TextStyle(color: Colors.black,fontSize: 20,fontWeight: FontWeight.bold),
+            ),
+          ),
+          new Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new Radio(
+                value: 0,
+                groupValue: _radioValue1,
+                onChanged: _handleRadioValueChange1,
+              ),
+              Row(
+                children: [
+                  Icon(Icons.public),
+                  Padding(
+                    padding: const EdgeInsets.only(left:4.0),
+                    child: new Text(
+                      'Public',
+                      style: new TextStyle(fontSize: 16.0),
+                    ),
+                  ),
+                ],
+              ),
+              new Radio(
+                value: 1,
+                groupValue: _radioValue1,
+                onChanged: _handleRadioValueChange1,
+              ),
+              Row(
+                children: [
+                  Icon(Icons.close),
+                  Padding(
+                    padding: const EdgeInsets.only(left:4.0),
+                    child: new Text(
+                      'Private',
+                      style: new TextStyle(
+                        fontSize: 16.0,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Padding(
+            padding:EdgeInsets.all(8.0),
+            child:Container(
+              height:1.0,
+              width:double.infinity,
+              color:Colors.black12,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              'Visibility',style: TextStyle(color: Colors.black,fontSize: 20,fontWeight: FontWeight.bold),
+            ),
+          ),
+          new Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new Radio(
+                value: 0,
+                groupValue: _radioValue2,
+                onChanged: _handleRadioValueChange2,
+              ),
+              Row(
+                children: [
+                  Icon(Icons.visibility),
+                  Padding(
+                    padding: const EdgeInsets.only(left:4.0),
+                    child: new Text(
+                      'Visible',
+                      style: new TextStyle(fontSize: 16.0),
+                    ),
+                  ),
+                ],
+              ),
+              new Radio(
+                value: 1,
+                groupValue: _radioValue2,
+                onChanged: _handleRadioValueChange2,
+              ),
+              Row(
+                children: [
+                  Icon(Icons.visibility_off),
+                  Padding(
+                    padding: const EdgeInsets.only(left:4.0),
+                    child: new Text(
+                      'Hidden',
+                      style: new TextStyle(
+                        fontSize: 16.0,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Padding(
+            padding:EdgeInsets.all(8.0),
+            child:Container(
+              height:1.0,
+              width:double.infinity,
+              color:Colors.black12,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              'Posts',style: TextStyle(color: Colors.black,fontSize: 20,fontWeight: FontWeight.bold),
+            ),
+          ),
+          new Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new Radio(
+                value: 0,
+                groupValue: _radioValue2,
+                onChanged: _handleRadioValueChange2,
+              ),
+              Row(
+                children: [
+                  Icon(Icons.person),
+                  Padding(
+                    padding: const EdgeInsets.only(left:4.0),
+                    child: new Text(
+                      'Admin',
+                      style: new TextStyle(fontSize: 16.0),
+                    ),
+                  ),
+                ],
+              ),
+              new Radio(
+                value: 1,
+                groupValue: _radioValue2,
+                onChanged: _handleRadioValueChange2,
+              ),
+              Row(
+                children: [
+                  Icon(Icons.people),
+                  Padding(
+                    padding: const EdgeInsets.only(left:4.0),
+                    child: new Text(
+                      'Everyone',
+                      style: new TextStyle(
+                        fontSize: 16.0,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Padding(
+            padding:EdgeInsets.all(8.0),
+            child:Container(
+              height:1.0,
+              width:double.infinity,
+              color:Colors.black12,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              'Verification',style: TextStyle(color: Colors.black,fontSize: 20,fontWeight: FontWeight.bold),
+            ),
+          ),
+          new Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new Radio(
+                value: 0,
+                groupValue: _radioValue2,
+                onChanged: _handleRadioValueChange2,
+              ),
+              Row(
+                children: [
+                  Icon(Icons.person),
+                  Padding(
+                    padding: const EdgeInsets.only(left:4.0),
+                    child: new Text(
+                      'Admin',
+                      style: new TextStyle(fontSize: 16.0),
+                    ),
+                  ),
+                ],
+              ),
+              new Radio(
+                value: 1,
+                groupValue: _radioValue2,
+                onChanged: _handleRadioValueChange2,
+              ),
+              Row(
+                children: [
+                  Icon(Icons.people),
+                  Padding(
+                    padding: const EdgeInsets.only(left:4.0),
+                    child: new Text(
+                      'Everyone',
+                      style: new TextStyle(
+                        fontSize: 16.0,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Padding(
+            padding:EdgeInsets.all(8.0),
+            child:Container(
+              height:1.0,
+              width:double.infinity,
+              color:Colors.black12,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: FlatButton(
+              child: Text(
+                'Create Community',
+                style: TextStyle(color: Colors.blueAccent),
+              ),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(5.0),
+                side: BorderSide(color: Colors.blue)
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
