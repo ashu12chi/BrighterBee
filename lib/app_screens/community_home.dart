9971357b8256ca9a55a4c434639770b56f7fc5f0
Ashutosh Chitranshi <45908393+ashu12chi@users.noreply.github.com>
2020-10-12 18:13:16 +0530

    Update community_home.dart

--- lib/app_screens/community_home.dart
+++ lib/app_screens/community_home.dart
@@ -11,8 +11,8 @@ class CommunityHome extends StatefulWidget {
 }
 
 class _CommunityHomeState extends State<CommunityHome> {
-  String mediaUrl =
-      'https://firebasestorage.googleapis.com/v0/b/brighterbee-npdevs.appspot.com/o/thumbnails%2Fthumbnail_video_default.png?alt=media&token=110cba28-6dd5-4656-8eca-cbefe9cce925';
+
+  String mediaUrl = 'https://firebasestorage.googleapis.com/v0/b/brighterbee-npdevs.appspot.com/o/thumbnails%2Fthumbnail_video_default.png?alt=media&token=110cba28-6dd5-4656-8eca-cbefe9cce925';
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
@@ -20,17 +20,9 @@ class _CommunityHomeState extends State<CommunityHome> {
     return Scaffold(
       appBar: AppBar(
         actions: <Widget>[
+          IconButton(icon: Icon(Icons.search,color: Colors.grey,),),
           IconButton(
-            icon: Icon(
-              Icons.search,
-              color: Colors.grey,
-            ),
-          ),
-          IconButton(
-            icon: Icon(
-              Icons.more_horiz,
-              color: Colors.grey,
-            ),
+            icon: Icon(Icons.more_horiz,color: Colors.grey,),
             onPressed: showOptions,
           )
         ],
@@ -40,9 +32,11 @@ class _CommunityHomeState extends State<CommunityHome> {
         child: Column(
           children: <Widget>[
             Padding(
-              padding: const EdgeInsets.only(top: 8.0),
+              padding:
+              const EdgeInsets.only(top: 8.0),
               child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
+                crossAxisAlignment:
+                CrossAxisAlignment.stretch,
                 children: [
                   Image.network(
                     mediaUrl,
@@ -53,43 +47,28 @@ class _CommunityHomeState extends State<CommunityHome> {
               ),
             ),
             Center(
-                child: Card(
-              child: InkWell(
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => CommunityProfile()));
-                },
-                child: Column(
-                  children: [
-                    Text(
-                      'Mathematics',
-                      style:
-                          TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-                    ),
+                child: Card (
+                  child: InkWell(
+                    onTap: (){
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => CommunityProfile()));
+                    },
+                    child: Column(
+              children: [
+                    Text('Mathematics',style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold),),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: <Widget>[
-                        Icon(
-                          Icons.public,
-                          color: Colors.grey,
-                          size: 15,
-                        ),
-                        Text(
-                          ' Public group  ',
-                          style: TextStyle(color: Colors.grey, fontSize: 15),
-                        ),
-                        Text(
-                          '12 Members',
-                          style: TextStyle(color: Colors.grey, fontSize: 15),
-                        )
+                        Icon(Icons.public,color: Colors.grey,size: 15,),
+                        Text(' Public group  ',style: TextStyle(color: Colors.grey,fontSize: 15),),
+                        Text('12 Members',style: TextStyle(color: Colors.grey,fontSize: 15),)
                       ],
                     )
-                  ],
-                ),
-              ),
-            )),
+              ],
+            ),
+                  ),
+                )
+            ),
             Padding(
               padding: EdgeInsets.only(top: 8.0, bottom: 8.0),
               child: Container(
@@ -99,35 +78,29 @@ class _CommunityHomeState extends State<CommunityHome> {
               ),
             ),
             Padding(
-              padding: const EdgeInsets.only(left: 4.0, right: 4.0),
+              padding: const EdgeInsets.only(left:4.0,right: 4.0),
               child: SizedBox(
                 height: 40,
                 width: double.infinity,
                 child: FlatButton(
-                  child: Text(
-                    'Write something here...',
-                    style: TextStyle(color: Colors.grey, fontSize: 18),
-                  ),
+                  child: Text('Write something here...',style: TextStyle(color: Colors.grey,fontSize: 18),),
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(20),
                     side: BorderSide(color: Colors.black12),
                   ),
-                  onPressed: () {},
+                  onPressed: (){
+
+                  },
                 ),
               ),
             ),
             StreamBuilder(
-              stream: FirebaseFirestore.instance
-                  .collection('communities')
-                  .doc('Mathematics')
-                  .collection('posts')
-                  .snapshots(),
-              builder: (context, snapshot) {
+              stream: FirebaseFirestore.instance.collection('communities').doc('Mathematics').collection('posts').snapshots(),
+              builder: (context,snapshot) {
                 return snapshot.connectionState == ConnectionState.waiting
                     ? Center(
                   child: CircularProgressIndicator(),
-                )
-                    : ListView.builder(
+                ):ListView.builder(
                   //scrollDirection: Axis.vertical,
                   physics: NeverScrollableScrollPhysics(),
                   shrinkWrap: true,
@@ -135,26 +108,29 @@ class _CommunityHomeState extends State<CommunityHome> {
                   itemBuilder: (context, index) {
                     DocumentSnapshot documentSnapshot =
                     snapshot.data.docs[index];
-                    print(documentSnapshot['title']);
-                    return Column(
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.only(top: 8.0, bottom: 8.0),
-                          child: Container(
-                            height: 5.0,
-                            width: double.infinity,
-                            color: Colors.black12,
+                    print(documentSnapshot.id);
+                    print('112');
+                      return Column(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.only(top: 8.0, bottom: 8.0),
+                            child: Container(
+                              height: 5.0,
+                              width: double.infinity,
+                              color: Colors.black12,
+                            ),
                           ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(
-                              left: 8.0, right: 8.0, top: 8.0),
-                          child: SizedBox(
-                              height: 530, child: PostCardView()),
-                        ),
-                      ],
-                    );
-                    //return Text('ashu12_chi');
+                          Padding(
+                            padding: const EdgeInsets.only(
+                                left: 8.0, right: 8.0, top: 8.0),
+                            child: SizedBox(
+                              height: 530,
+                              child: PostCardView('Mathematics',documentSnapshot.id)
+                            ),
+                          ),
+                        ],
+                      );
+                  //return Text('ashu12_chi');
                   },
                 );
               },
@@ -164,6 +140,7 @@ class _CommunityHomeState extends State<CommunityHome> {
       ),
     );
   }
+
 
   showOptions() {
     showModalBottomSheet(
@@ -181,51 +158,29 @@ class _CommunityHomeState extends State<CommunityHome> {
                       children: <Widget>[
                         Column(
                           children: <Widget>[
-                            IconButton(
-                                icon: Icon(
-                                  Icons.share,
-                                  size: 30,
-                                  color: Colors.black,
-                                )),
-                            Text(
-                              'Share',
-                              style: TextStyle(fontSize: 18),
-                            ),
+                            IconButton(icon: Icon(Icons.share,size: 30,color: Colors.black,)),
+                            Text('Share',style: TextStyle(fontSize: 18),),
                           ],
                         ),
                         Column(
                           children: <Widget>[
-                            IconButton(
-                                icon: Icon(
-                                  Icons.exit_to_app,
-                                  size: 30,
-                                  color: Colors.black,
-                                )),
-                            Text(
-                              'Leave Community',
-                              style: TextStyle(fontSize: 18),
-                            ),
+                            IconButton(icon: Icon(Icons.exit_to_app,size: 30,color: Colors.black,)),
+                            Text('Leave Community',style: TextStyle(fontSize: 18),),
                           ],
                         ),
                         Column(
                           children: <Widget>[
-                            IconButton(
-                                icon: Icon(
-                                  Icons.report,
-                                  size: 30,
-                                  color: Colors.black,
-                                )),
-                            Text(
-                              'Report',
-                              style: TextStyle(fontSize: 18),
-                            ),
+                            IconButton(icon: Icon(Icons.report,size: 30,color: Colors.black,)),
+                            Text('Report',style: TextStyle(fontSize: 18),),
                           ],
                         )
                       ],
                     ),
                   ),
                 );
-              });
-        });
+              }
+          );
+        }
+    );
   }
 }
