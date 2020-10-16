4ea244d16e881c833211e59e40478d1eb6d6ad17
ashu12chi <ashutosh7234@gmail.com>
2020-10-16 20:28:06 +0530

    Device ID removed from typscript code

--- CloudFunctions/functions/src/index.ts
+++ CloudFunctions/functions/src/index.ts
@@ -27,8 +27,6 @@ export const sendToDevice = functions.firestore
     var id2 = id.concat(name.toString());
 
      const tokens = querySnapshot.docs.map(snap => snap.id);
-  
-    //var tokens = ['c3tlmM7fSdq97Cw12e2qLS:APA91bEJHdYII8FahAdLE3ugnnPlrW7HClGjVGPuZsoj5bn4Tbsvs0pEMRpngB//cnF5HFQM1JQnJHk1TlwRXrgXGXzmX09r49lNg4IQPzqZAQv_7l5OMVNX8nGnMxEvYMum7wn0hhkuD0'];
 
     const payload: admin.messaging.MessagingPayload = {
       notification: {
@@ -47,21 +45,3 @@ export const sendToDevice = functions.firestore
     return fcm.sendToDevice(tokens, payload);
   });
 
-
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
-// In the notification collection in firestore following keys to be added:
-// 1. receiver : person who will recieve notification
-// 2. title : title of notification
-// 3. body : Body of notification
-// 4. community : Community in which that post belong
-// 5. postId : Post Id.
-// 6. creator: creator of Post
-
-// Last three are for PostUI constructor 
